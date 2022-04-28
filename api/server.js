c9ed63cf3b90a1449001ed4c6cda7a042879012b
DarkedgeTH <darkedgeth@gmail.com>
2022-04-28 14:27:22 -0400

    set up server

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const Pokemon = require()
+const Pokemon = require('./pokemon/pokemon-model')
 
 const server = express();
 
@@ -9,5 +9,58 @@ server.use(express.json());
 server.get("/", (req, res) => {
     res.status(200).json({ api: "up" });
 });
+
+server.get("/pokemon", (req, res) => {
+    Pokemon.getAll()
+      .then(pokemon => {
+        res.status(200).json(pokemon);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  server.get("/pokemon/:id", (req, res) => {
+    Pokemon.getById(req.params.id)
+      .then(pokemon => {
+        if(pokemon == null) {
+          res.status(404).json({ message: 'Pokemon not found' });
+          return;
+        }
+        res.status(200).json(pokemon);
+      });
+  });
+  
+  server.post("/pokemon", (req, res) => {
+    Pokemon.insert(req.body)
+      .then(pokemon => {
+        res.status(201).json(pokemon);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      })
+  });
+  
+  server.delete("/pokemon/:id", (req, res) => {
+    Pokemon.remove(req.params.id)
+      .then(pokemon => {
+        if(pokemon == null) {
+          res.status(404).json({ message: 'Pokemon not found' });
+          return;
+        }
+        res.status(200).json(pokemon);
+      });
+  });
+  
+  server.put("/pokemon/:id", (req, res) => {
+    Pokemon.update(req.params.id, req.body)
+      .then(pokemon => {
+        if(pokemon == null) {
+          res.status(404).json({ message: 'Pokemon not found' });
+          return;
+        }
+        res.status(200).json(pokemon);
+      })
+  });
   
 module.exports = server;
