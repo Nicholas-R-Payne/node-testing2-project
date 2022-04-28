e18e4b5c928231abc055895f08e3b4b68b0e6671
DarkedgeTH <darkedgeth@gmail.com>
2022-04-28 14:35:40 -0400

    server and getAll tests working

--- api/server.test.js
+++ api/server.test.js
@@ -1,12 +1,39 @@
 const server = require('./server')
 const request = require('supertest')
+const db = require('../data/db-config')
+const Pokemon = require('./pokemon/pokemon-model')
 
-test('make sure our environment is set correctly', () => {
-    expect(process.env.NODE_ENV).toBe('testing');
+beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
 });
+
+beforeEach(async () => {
+    await db('pokemon').truncate();
+    await db('pokemon')
+        .insert([
+            { name: 'Bulbasaur' },
+            { name: 'Charmander' },
+            { name: 'Squirtle' },
+            { name: 'Pikachu' }
+        ])
+});
+
+afterAll(async () => {
+    await db.destroy()
+})
 
 test('server is up', async () => {
     const res = await request(server).get('/')
     expect(res.status).toBe(200)
     expect(res.body).toEqual({ api: 'up' })
+})
+
+describe('database tests', () => {
+    test('getAll', async () => {
+        const result = await Pokemon.getAll();
+        expect(result.constructor.name).toBe('Array');
+        expect(result.length).toBe(4);
+        expect(result[1]).toMatchObject({ name: 'Charmander' });
+    });
 })
